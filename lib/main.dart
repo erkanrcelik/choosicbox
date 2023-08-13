7393b25a319644a38732cc5ed1918838bd76e9bb
Mikail Yusuf Gulluk <gullukmikail@gmail.com>
2023-08-14 00:01:34 +0300

    paketlerin yuklenmis hali

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,14 @@
+import 'package:choosicbox/modules/Home/View/home.dart';
 import 'package:choosicbox/modules/Login/View/login.dart';
-import 'package:choosicbox/modules/Main/View/home.dart';
 import 'package:choosicbox/modules/Profile/View/profile.dart';
+import 'package:choosicbox/modules/Register/View/register.dart';
 
 import 'package:flutter/material.dart';
 
-import 'modules/Register/View/register.dart';
+import 'modules/Category/View/category.dart';
 
 void main() {
-  runApp(MyApp(registerScreen: ProfileScreen()));
+  runApp(MyApp(registerScreen: HomeScreen()));
 }
 
 class MyApp extends StatelessWidget {
@@ -19,7 +20,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: ProfileScreen(),
+      home: HomeScreen(),
     );
   }
 }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,14 @@
 import FlutterMacOS
 import Foundation
 
+import awesome_notifications
+import firebase_auth
+import firebase_core
+import firebase_storage
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  AwesomeNotificationsPlugin.register(with: registry.registrar(forPlugin: "AwesomeNotificationsPlugin"))
+  FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
+  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  FLTFirebaseStoragePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseStoragePlugin"))
 }
