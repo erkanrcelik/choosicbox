d1e87aeea4c9489028fcd21c026fab0e17c36c70
Mikail Yusuf Gulluk <gullukmikail@gmail.com>
2023-08-14 00:50:47 +0300

    some updates

--- lib/modules/Home/View/home.dart
+++ lib/modules/Home/View/home.dart
@@ -1,5 +1,14 @@
 import 'package:flutter/material.dart';
 
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomeScreen(),
+    );
+  }
+}
+
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -8,42 +17,20 @@ class HomeScreen extends StatelessWidget {
         child: Column(
           children: [
             SizedBox(height: 20),
-            Container(
-              padding: const EdgeInsets.all(20),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Expanded(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Container(
-                          padding: const EdgeInsets.only(bottom: 8),
-                          child: const Text(
-                            "Location",
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                        ),
-                        Row(
-                          children: [
-                            Text(
-                              "Current location",
-                              style: TextStyle(
-                                color: Colors.grey[500],
-                              ),
-                            ),
-                            SizedBox(width: 8),
-                          ],
-                        ),
-                      ],
-                    ),
-                  ),
-                  Container(
-                    alignment: Alignment.center, // Align text in the middle
-                    child: const Text(
-                      "CHOOSICBOX",
+            TitleSection(),
+            WelcomeSection(),
+            SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                InkWell(
+                  onTap: () {
+                    // Handle "All" option click
+                  },
+                  child: Padding(
+                    padding: EdgeInsets.only(left: 20.0,),
+                    child: Text(
+                      "All",
                       style: TextStyle(
                         color: Color(0xFF121212),
                         fontSize: 14,
@@ -52,25 +39,205 @@ class HomeScreen extends StatelessWidget {
                       ),
                     ),
                   ),
-                  Container(
-                    width: 37.17,
-                    height: 55.76,
-                    decoration: const BoxDecoration(
-                      shape: BoxShape.circle,
-                      image: DecorationImage(
-                        image: NetworkImage("https://via.placeholder.com/37x56"),
-                        fit: BoxFit.fill,
+                ),
+                InkWell(
+                  onTap: () {
+                    // Handle "Popular" option click
+                  },
+                  child: Padding(
+                    padding: EdgeInsets.only(left: 60.0, ),
+                    child: Text(
+                      "Popular",
+                      style: TextStyle(
+                        color: Color(0xFF121212),
+                        fontSize: 14,
+                        fontFamily: 'Poppins',
+                        fontWeight: FontWeight.w700,
                       ),
                     ),
                   ),
-                ],
-              ),
+                ),
+                InkWell(
+                  onTap: () {
+                    // Handle "Near" option click
+                  },
+                  child: Padding(
+                    padding: EdgeInsets.only(left: 60.0, ),
+                    child: Text(
+                      "Near",
+                      style: TextStyle(
+                        color: Color(0xFF121212),
+                        fontSize: 14,
+                        fontFamily: 'Poppins',
+                        fontWeight: FontWeight.w700,
+                      ),
+                    ),
+                  ),
+                ),
+                InkWell(
+                  onTap: () {
+                    // Handle "Recommended" option click
+                  },
+                  child: Padding(
+                    padding: EdgeInsets.only(left: 60.0, ),
+                    child: Text(
+                      "Recommended",
+                      style: TextStyle(
+                        color: Color(0xFF121212),
+                        fontSize: 14,
+                        fontFamily: 'Poppins',
+                        fontWeight: FontWeight.w700,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
             ),
             SizedBox(height: 20),
-            // Add other containers here
+            StoreSection(),
+            // Add other widgets here
           ],
         ),
       ),
     );
   }
 }
+
+class TitleSection extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(20),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Container(
+            alignment: Alignment.center, // Align text in the middle
+            child: const Text(
+              "CHOOSICBOX",
+              style: TextStyle(
+                color: Color(0xFF121212),
+                fontSize: 14,
+                fontFamily: 'Poppins',
+                fontWeight: FontWeight.w700,
+              ),
+            ),
+          ),
+          Container(
+            width: 37.17,
+            height: 55.76,
+            decoration: const BoxDecoration(
+              shape: BoxShape.circle,
+              image: DecorationImage(
+                image: NetworkImage("https://via.placeholder.com/37x56"),
+                fit: BoxFit.fill,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+class WelcomeSection extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(20),
+      child: Row(
+        children: [
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "Welcome to Istanbul",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black87,
+                    fontSize: 18,
+                  ),
+                ),
+                SizedBox(height: 10),
+                Text(
+                  "Let's find your correct place",
+                  style: TextStyle(
+                    color: Colors.grey[500],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+class StoreSection extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      child: Container(
+        padding: const EdgeInsets.all(10),
+        child: Row(
+          children: [
+            for (int index = 0; index < 5; index++)
+              Container(
+                margin: const EdgeInsets.only(right: 10),
+                child: GestureDetector(
+                  onTap: () {
+                    // Handle card click action here
+                  },
+                  child: Card(
+                    elevation: 3,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Container(
+                      width: 150,
+                      height: 200,
+                      padding: const EdgeInsets.all(10),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(
+                                "Store $index",
+                                style: const TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 16,
+                                ),
+                              ),
+                              Icon(
+                                Icons.location_on,
+                                color: Colors.blue[500],
+                              ),
+                            ],
+                          ),
+                          const Spacer(),
+                          Text(
+                            "Address or Description",
+                            style: TextStyle(
+                              color: Colors.grey[500],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
+
+
+
